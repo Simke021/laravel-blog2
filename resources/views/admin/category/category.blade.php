853824e1453c25c3ba171cdd7b5b5505359e577f
Aleksandar Ljubišić <simke20@yahoo.com>
2017-08-21 22:03:04 +0200

    Routes and views for Admin pannel

---
+++ resources/views/admin/category/category.blade.php
@@ -0,0 +1,55 @@
+@extends('admin.layouts.app')
+
+@section('main-content')	
+  
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Text Editors
+        <small>Advanced form element</small>
+      </h1>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Titles</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="row">
+                <div class="col-lg-6 col-lg-offset-3">
+                  <div class="box-body">
+
+                    <div class="form-group">
+                      <label for="name">Category title:</label>
+                      <input type="text" name="name" class="form-control" id="name" placeholder="Post category ...">
+                    </div>
+
+                    <div class="form-group">
+                      <label for="slug">Category slug:</label>
+                      <input type="text" name="slug" class="form-control" id="slug" placeholder="Category URL slug ...">
+                    </div>
+
+                    <div class="form-group">
+	                <button type="submit" class="btn btn-primary">Submit</button>
+	              </div>
+                </div>
+
+			  </div><!-- End of col-lg-6 --> 
+            </form>
+          </div>
+        </div>
+        <!-- /.col-->
+      </div>
+      <!-- ./row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,19 @@ Route::get('/', function () {
 Route::get('post', function(){
 	return view('user.post');
 })->name('post');
+
+Route::get('admin/home', function(){
+	return view('admin.home');
+})->name('post');
+
+Route::get('admin/post', function(){
+	return view('admin.post.post');
+});
+
+Route::get('admin/tag', function(){
+	return view('admin.tag.tag');
+});
+
+Route::get('admin/category', function(){
+	return view('admin.category.category');
+});
