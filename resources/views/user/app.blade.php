d2194ddc1b4745981cb99df505169ebed541bd94
Aleksandar Ljubišić <simke20@yahoo.com>
2017-08-21 13:24:44 +0200

    First steps

---
+++ resources/views/user/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    @include('user.layouts.head')
+
+</head>
+
+<body>
+
+    @include('user.layouts.nav')
+
+    @section('main-content')
+
+        @show
+
+    @include('user.layouts.footer')
+
+</body>
+
+</html>
+   
