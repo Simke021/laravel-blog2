cd466b5ec59336ff4fcd5ff9e8e97e7104950842
Aleksandar Ljubišić <simke20@yahoo.com>
2017-08-24 01:55:21 +0200

    Add Facebook comment section for posts

--- resources/views/user/post.blade.php
+++ resources/views/user/post.blade.php
@@ -9,7 +9,18 @@
 @section('main-content')
 	
 	<!-- Post Content -->
+
+<!-- Facebook Comments -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/sr_RS/sdk.js#xfbml=1&version=v2.10&appId=265648440606199";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
     <article>
+    
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
@@ -27,6 +38,9 @@
                         <small style="margin-right: 10px; border-radius: 5px; border: 1px solid gray; padding: 5px;"><a href="#">{{ $tag->name }}</a></small>
                     @endforeach 
                 </div>
+                
+                <div class="fb-comments" data-href="{{ Request::url() }}" data-numposts="5"></div>
+                
             </div>
         </div>
     </article>
